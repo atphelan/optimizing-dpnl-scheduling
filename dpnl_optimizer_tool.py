1866cf32a7fcaad560347dfd0ef54a708f8353c0
Alastar Phelan <15atphelan@gmail.com>
2026-01-19 18:24:44 +0000

    renaming for consistency

--- dpnl_optimizer_tool.py
+++ dpnl_optimizer_tool.py
@@ -6,7 +6,7 @@ gates = ['EdU+BrdU-', 'EdU+BrdU+', 'EdU-BrdU+']
 def find_s_optimum_from_counts(dt: int, edu_sp: int, dp: int, brdu_sp: int, print_only=False):
     '''
     Function takes the following:
-    dt - the waiting time between labels used in a pilot dual nucleoside labeling assay
+    dt - the waiting time between labels used in a pilot dual nucleoside labeling assay. Must be to the nearest hour.
     edu_sp - the number of EdU single-positive cells counted as a raw number or percentage of total cells counted
     dp - the number of double-positive cells counted, processed as above
     brdu_sp - the number of BrdU single-positive cells counted, processed as above
@@ -50,12 +50,12 @@ def find_s_optimum_from_counts(dt: int, edu_sp: int, dp: int, brdu_sp: int, prin
     else:
         return dt_opt, max_snr
 
-def find_s_optimum_from_phases(tg1: float, ts: float, tp: float, print_only=False):
+def find_s_optimum_from_phases(tg1: float, ts: float, tc: float, print_only=False):
     '''
     Function takes the following:
     tg1 - estimated duration of G1 phase
     ts - estimated duration of S phase
-    tp - estimated duration of whole cell cycle
+    tc - estimated duration of whole cell cycle
     Units are not important as long as they are consistent, but hours are expected.
 
     The function then returns a recommendation for the time interval to use, based on maximal Signal-to-Noise Ratio in our simulation data.
@@ -66,9 +66,9 @@ def find_s_optimum_from_phases(tg1: float, ts: float, tp: float, print_only=Fals
     '''
     assert 0 < tg1
     assert 0 < ts
-    assert 0 < tp
+    assert 0 < tc
 
-    relative_times = np.array([tg1, ts, tp-tg1-ts])/tp
+    relative_times = np.array([tg1, ts, tc-tg1-ts])/tc
 
     # Reference simulations generated using the stochastic 3-stage cell-cycle model described in the manuscript,
     # spanning plausible parameter combinations and inter-label waiting times for a 24-hour cycle.
@@ -89,12 +89,58 @@ def find_s_optimum_from_phases(tg1: float, ts: float, tp: float, print_only=Fals
                                                                                                                 ))]
     max_snr = comparable_series['SNR k2'].max()
     dt_opt = comparable_series[comparable_series['SNR k2'] == max_snr]['BrdU time'].iloc[0]
-    dt_opt = int(round(dt_opt * tp/24))
+    dt_opt = int(round(dt_opt * tc/24))
 
     if print_only:
-        readout(dt_opt, max_snr)
+        readout(dt_opt, max_snr, counts=False)
     else:
         return dt_opt, max_snr
+
+def find_s_optimum_from_phases_via_counts(dt: float, edu_sp: float, dp: float, brdu_sp: float, dn_active: float, print_only=False):
+    '''
+    Function takes the following:
+    dt - the waiting time between labels used in a pilot dual nucleoside labeling assay
+    edu_sp - the number of EdU single-positive cells counted as a raw number or percentage of total cells counted
+    dp - the number of double-positive cells counted, processed as above
+    brdu_sp - the number of BrdU single-positive cells counted, processed as above
+    dn_active - the nubmer of unlabeled cells beleived to be in G1/G2/M phase counted.
+    
+    The function then returns a recommendation for the time interval to use, based on maximal Signal-to-Noise Ratio in our simulation data.
+    Recommendations are based on the closest match to the ratios of EdU+BrdU-, EdU-BrdU+, and EdU+BrdU+ cells 
+    observed in simulations of continuously proliferating cells.
+    Caution should be taken when using the results on cell populations that do not match this profile.
+    The tool does not perform parameter inference itself, nor any interpolation.
+    '''    
+    
+    # # From Harris et al., 2018. Appropriate only if cell cycle progression is reasonably clock-like with negligible growth in population.
+    # ts_estimate = dt * (dp)/edu_sp # Only valid if edu_sp divisions are minimal or corrected for in the provided count
+    # tc_estimate = ts_estimate * (edu_sp + dn_active)/(dp + brdu_sp)
+
+    # Formulae derived assuming exponentially greater density of cells earlier in the cycle with the boundary condition that n(t_c) = 0.5 * n(0)
+    ts_estimate = dt / np.log((edu_sp+dp)/dp)
+    tc_estimate = ts_estimate * (brdu_sp + dn_active)/(edu_sp+dp)
+    print(tc_estimate)
+
+
+    reference_df = pd.read_json('DPNL inference results.json')
+
+    lookup_slice = reference_df[reference_df['BrdU time']==2].copy() # Picking a specific waiting time to narrow down number of rows to initially check
+    # Scaled Euclidean distance emphasizes relative proportions rather than absolute counts, helping to generalize the tool
+    lookup_slice['Relative ts difference'] = ((lookup_slice['ts'] - 24.0*ts_estimate/tc_estimate)/lookup_slice['ts'])**2
+    nearest_row = lookup_slice.iloc[lookup_slice['Relative ts difference'].argmin(skipna=True)]
+    comparable_series = reference_df[np.logical_and(np.isclose(reference_df['k1'], nearest_row['k1']), np.isclose(reference_df['k2'], nearest_row['k2']
+                                                                                                                ))]
+    max_snr = comparable_series['SNR k2'].max()
+    dt_opt = comparable_series[comparable_series['SNR k2'] == max_snr]['BrdU time'].iloc[0]
+    dt_opt = int(round(dt_opt * tc_estimate/24))
+
+    if print_only:
+        readout(dt_opt, max_snr, counts=False)
+        print('Phase times were deduced from counts using an exponentially growing population cell cycle model.')
+    else:
+        return dt_opt, max_snr
+
+
 
 def find_custom_optimum_from_counts(dt: int, edu_sp: int, dp: int, brdu_sp: int, g1_weight: float, s_weight: float, print_only=False):
     '''
@@ -152,12 +198,12 @@ def find_custom_optimum_from_counts(dt: int, edu_sp: int, dp: int, brdu_sp: int,
         return dt_opt, max_snr
 
 
-def find_custom_optimum_from_phases(tg1: float, ts: float, tp: float, g1_weight: float, s_weight: float, print_only=False):
+def find_custom_optimum_from_phases(tg1: float, ts: float, tc: float, g1_weight: float, s_weight: float, print_only=False):
     '''
     Function takes the following:
     tg1 - estimated duration of G1 phase
     ts - estimated duration of S phase
-    tp - estimated duration of whole cell cycle
+    tc - estimated duration of whole cell cycle
     g1_weight - the relative contribution of SNR k_g1 to the objective SNR
     s_weight - contribution of SNR k_S to the objective SNR.
     Note g1 inference should be prioritised if g2m phase time is needed - s phase is always well resolved in comparison so g2m can be inferred from g1 + s.
@@ -172,14 +218,14 @@ def find_custom_optimum_from_phases(tg1: float, ts: float, tp: float, g1_weight:
     '''
     assert 0 < tg1
     assert 0 < ts
-    assert 0 < tp
+    assert 0 < tc
     assert 0 <= g1_weight
     assert 0 <= s_weight
     assert g1_weight + s_weight > 0
 
     g1_weight, s_weight = np.array([g1_weight, s_weight])/sum([g1_weight, s_weight]) # Normalization
 
-    relative_times = np.array([tg1, ts, tp-tg1-ts])/tp
+    relative_times = np.array([tg1, ts, tc-tg1-ts])/tc
 
     # Reference simulations generated using the stochastic 3-stage cell-cycle model described in the manuscript,
     # spanning plausible parameter combinations and inter-label waiting times for a 24-hour cycle.
@@ -201,17 +247,17 @@ def find_custom_optimum_from_phases(tg1: float, ts: float, tp: float, g1_weight:
                                                                                                                 ))]
     max_snr = comparable_series['Objective SNR'].max()
     dt_opt = comparable_series[comparable_series['Objective SNR'] == max_snr]['BrdU time'].iloc[0]
-    dt_opt = int(round(dt_opt * tp/24))
+    dt_opt = int(round(dt_opt * tc/24))
 
     if print_only:
-        readout(dt_opt, max_snr)
+        readout(dt_opt, max_snr, counts=False)
     else:
         return dt_opt, max_snr
 
 
-def readout(dt_opt, max_snr, dt=0):
+def readout(dt_opt, max_snr, dt=0, counts=True):
 
-    print(f'A time of {dt_opt} hours is recommended, for which we found a SNR of {round(max_snr, 1)} for similar labeled cell proportions in our simulation study.')
+    print(f'A time of {dt_opt} hours is recommended, for which we found a SNR of {round(max_snr, 1)} for similar {'labeled cell' if counts else 'phase duration'} proportions in our simulation study.')
     if dt == dt_opt:
         print('It looks like you already used the ideal timing!')
 
@@ -220,30 +266,32 @@ if __name__ == '__main__':
     ### DEMO ###
     print('1.')
     find_s_optimum_from_counts(dt=2, edu_sp=6.3, dp=23.0, brdu_sp=6.6, print_only=True) # Numbers as used in the publication Figure 1c, which are Leukemic Stem-Like cells in Bone marrow.
-    # The tool recommends 4 hours, which would balance the DP and SP population sizes more.
-    print('2.')
-    find_s_optimum_from_counts(dt=4, edu_sp=1, dp=1, brdu_sp=1, print_only=True) # Trial numbers
-    # The tool recommends 2 hours, which would give fewer SP cells but more DP cells.
-    # This suggests a nontrivial Signal-to-Noise Ratio optimization trade-off not previously considered by researchers.
-    print('3.')
-    find_custom_optimum_from_counts(dt=2, edu_sp=6.3, dp=23.0, brdu_sp=6.6, g1_weight=1.0, s_weight=0.0, print_only=True) # Optimize for k_g1 instead
-    print('4.')
-    find_custom_optimum_from_counts(dt=2, edu_sp=6.3, dp=23.0, brdu_sp=6.6, g1_weight=0.7, s_weight=0.3, print_only=True) # Optimize for 70% k_g1, 30% k_s instead
+    # # The tool recommends 4 hours, which would balance the DP and SP population sizes more.
+    # print('2.')
+    # find_s_optimum_from_counts(dt=4, edu_sp=1, dp=1, brdu_sp=1, print_only=True) # Trial numbers
+    # # The tool recommends 2 hours, which would give fewer SP cells but more DP cells.
+    # # This suggests a nontrivial Signal-to-Noise Ratio optimization trade-off not previously considered by researchers.
+    # print('3.')
+    # find_custom_optimum_from_counts(dt=2, edu_sp=6.3, dp=23.0, brdu_sp=6.6, g1_weight=1.0, s_weight=0.0, print_only=True) # Optimize for k_g1 instead
+    # print('4.')
+    # find_custom_optimum_from_counts(dt=2, edu_sp=6.3, dp=23.0, brdu_sp=6.6, g1_weight=0.7, s_weight=0.3, print_only=True) # Optimize for 70% k_g1, 30% k_s instead
 
-    print('5.')
-    find_s_optimum_from_counts(dt=2, edu_sp=3.22, dp=13.2, brdu_sp=4.75, print_only=True) # MPP cells from Akinduro et al., 2018.
-    print('6.')
-    find_s_optimum_from_counts(dt=2, edu_sp=1.18, dp=1.40, brdu_sp=1.33, print_only=True) # ST-HSC cells from Akinduro et al., 2018.
-    print('7.')
-    find_s_optimum_from_counts(dt=2, edu_sp=1.09, dp=6.10, brdu_sp=2.18, print_only=True) # HSC cells from Akinduro et al., 2018.
-    # Note the large differences between EdU+BrdU- and EdU-BrdU+ cells - this implies many newly divided cells are differentiating or exiting the BM before harvest.
-    # These large differences constitute an example when the optimization tool is not recommended for use.
+    # print('5.')
+    # find_s_optimum_from_counts(dt=2, edu_sp=3.22, dp=13.2, brdu_sp=4.75, print_only=True) # MPP cells from Akinduro et al., 2018.
+    # print('6.')
+    # find_s_optimum_from_counts(dt=2, edu_sp=1.18, dp=1.40, brdu_sp=1.33, print_only=True) # ST-HSC cells from Akinduro et al., 2018.
+    # print('7.')
+    # find_s_optimum_from_counts(dt=2, edu_sp=1.09, dp=6.10, brdu_sp=2.18, print_only=True) # HSC cells from Akinduro et al., 2018.
+    # # Note the large differences between EdU+BrdU- and EdU-BrdU+ cells - this implies many newly divided cells are differentiating or exiting the BM before harvest.
+    # # These large differences constitute an example when the optimization tool is not recommended for use.
 
-    print('8.')
-    find_s_optimum_from_phases(tg1=4, ts=4.5, tp=8.5, print_only=True) # Mean numbers for all cells in bone marrow (Frindel, Generation cycle of mouse bone marrow, 1967)
-    # The tool recommends 2 hours, which conveniently is the most commonly used timing for studies on mouse cells.
-    print('9.')
-    find_custom_optimum_from_phases(tg1=4, ts=4.5, tp=8.5, g1_weight=1.0, s_weight=0.0, print_only=True) # Same numbers, now optimize k_g1
-    print('10.')
-    find_custom_optimum_from_phases(tg1=4, ts=4.5, tp=8.5, g1_weight=0.7, s_weight=0.3, print_only=True) # Same numbers, now optimize 70% k_g1 & 30% k_s
+    # print('8.')
+    # find_s_optimum_from_phases(tg1=4, ts=4.5, tc=8.5, print_only=True) # Mean numbers for all cells in bone marrow (Frindel, Generation cycle of mouse bone marrow, 1967)
+    # # The tool recommends 2 hours, which conveniently is the most commonly used timing for studies on mouse cells.
+    # print('9.')
+    # find_custom_optimum_from_phases(tg1=4, ts=4.5, tc=8.5, g1_weight=1.0, s_weight=0.0, print_only=True) # Same numbers, now optimize k_g1
+    # print('10.')
+    # find_custom_optimum_from_phases(tg1=4, ts=4.5, tc=8.5, g1_weight=0.7, s_weight=0.3, print_only=True) # Same numbers, now optimize 70% k_g1 & 30% k_s
 
+    print('11.')
+    find_s_optimum_from_phases_via_counts(dt=2, edu_sp=6.30, dp=23.0, brdu_sp=6.59, dn_active=63.3, print_only=True)
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from dpnl_optimizer_tool import find_s_optimum_from_counts, find_s_optimum_from_phases
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,5 +18,5 @@ def optimize_counts(dt: int, edu: float, dp: float, brdu: float):
 
 @app.get("/optimize/phases")
 def optimize_phases(tg1: float, ts: float, tp: float):
-    result = find_s_optimum_from_phases(tg1, ts, tp)
+    result = find_s_optimum_from_phases(tg1, ts, tc)
     return {"Optimal t_wait": int(result[0]), "SNR k2 from simulation": float(result[1])}
