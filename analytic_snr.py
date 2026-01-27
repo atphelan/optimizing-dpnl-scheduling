sensitivity + normalization fix

analytic_snr.py
@@ -8,6 +8,8 @@ from gECC import hex_param_mesh
 from itertools import product as iprod
 from gECC import generate_steady_state
 
+from datetime import datetime
+
 def build_base_matrices():
     n = 3
     S_base = np.array([[-1,  0, +2],
@@ -344,10 +346,11 @@ def sweep(template_df: pd.DataFrame = None):
     if template_df is None:
         df_gill = pd.read_json('DPNL inference results.json')
         df_new = df_gill[['k1', 'k2', 'k3', 'BrdU time', 'Initial G1', 'Initial S', 'Initial G2M']].copy()
-        fname = 'DPNL lookup.json'
+        fname = f'data/DPNL lookup {datetime.today().date()}.json'
     else:
         df_new = template_df[['k1', 'k2', 'k3', 'BrdU time', 'Initial G1', 'Initial S', 'Initial G2M']].copy()
-        fname = 'data/DL analytic cov templated.json'
+        df_new[['Initial G1', 'Initial S', 'Initial G2M']] = df_new[['Initial G1', 'Initial S', 'Initial G2M']]*2
+        fname = f'data/DL analytic cov templated {datetime.today().date()}.json'
     df_new['Covariance matrix'] = None
     for i, df_row in df_new.iterrows():
         k = df_row[['k1', 'k2', 'k3']]
@@ -389,9 +392,9 @@ def noisy_sweep():
 
 #%%
 if __name__ == '__main__':
-    # sweep()
+    sweep(template_df = pd.read_json('DPNL inference results.json'))
     # noisy_sweep()
-    # exit()
+    exit()
 
     k_points = hex_param_mesh(np.array([11,8,5]), 1/np.sqrt(6)*np.array([2,-1,-1]), 1/np.sqrt(6)*np.array([-1,-1,2])*0.5, 6)
     params = [{'k1': ks[0], 'k2': ks[1], 'k3': ks[2]} for ks in k_points]

gECC.py
@@ -443,7 +443,8 @@ class DualLabelingBatch(DualLabelingEnsemble):
             # new_row = gp_df.agg(agg)
             mean_row = pd.DataFrame(gp_df.agg(agg).iloc[0]).T
             std_row = pd.DataFrame(gp_df.agg(agg).iloc[1]).T
-            mean_row[[c.replace('Mean', 'Std') for c in corr_cols if 'Mean' in c]] = np.array(np.sqrt(self.repeats)*std_row[[c for c in corr_cols if 'Mean' in c]]) # Adjust ste to std
+            # mean_row[[c.replace('Mean', 'Std') for c in corr_cols if 'Mean' in c]] = np.array(np.sqrt(self.repeats)*std_row[[c for c in corr_cols if 'Mean' in c]]) # Adjust ste to std
+            mean_row[[c.replace('Mean', 'Std') for c in corr_cols if 'Mean' in c]] = np.array(std_row[[c for c in corr_cols if 'Mean' in c]]) # No adjustment
             matrix = np.corrcoef(gp_df[corr_cols[:-2]].copy().T).astype(object)
             mean_row['Ensemble Correlations'] = [matrix]
             compacted_df = pd.concat([compacted_df, mean_row.reset_index(drop=True)], axis='index')
@@ -922,7 +923,7 @@ def run_bootstrap(lookup_path, jobs=1, noisy_initial=False, cov_file=None, ks_as
         x_0=np.array([0]*12),
         extras=[jobs] # Jobs
     )
-    repeat_ensembles = [1000]
+    repeat_ensembles = [400]
 
     if lookup_path[-4:] == '.csv':
         DoubleStainBatch.lookup_df = pd.read_csv(lookup_path)
@@ -933,6 +934,7 @@ def run_bootstrap(lookup_path, jobs=1, noisy_initial=False, cov_file=None, ks_as
 
     if ks_as_in_lookup:
         k_points = DoubleStainBatch.lookup_df[['k1', 'k2', 'k3']].copy()
+        # Take 5% of points around the mean values of k1 and k2
         k_points = k_points[np.logical_and(np.isclose(k_points['k1'], k_points['k1'].mean(), rtol=5E-2), np.isclose(k_points['k2'], k_points['k2'].mean(), rtol=5E-2))].to_numpy()
     else:
         k_points = hex_param_mesh(np.array([11,8,5]), 1/np.sqrt(6)*np.array([2,-1,-1]), 1/np.sqrt(6)*np.array([-1,-1,2])*0.5, 6)
@@ -954,12 +956,12 @@ def run_bootstrap(lookup_path, jobs=1, noisy_initial=False, cov_file=None, ks_as
         cov_file = cov_file
     )
     try:
-        DoubleStainBatch.summary_data.to_json(f"data/DL bootstrap {datetime.today().date()} {int(os.environ['PBS_ARRAY_INDEX'])} {f'noisy {DoubleStainBatch.alpha}'.replace('.', 'p') if noisy_initial else ''} 10rep.json")
+        DoubleStainBatch.summary_data.to_json(f"data/DL bootstrap {datetime.today().date()} {int(os.environ['PBS_ARRAY_INDEX'])} {f'noisy {DoubleStainBatch.alpha}'.replace('.', 'p') if noisy_initial else ''} base.json")
     except:
-        DoubleStainBatch.summary_data.to_json(f"data/DL bootstrap {datetime.today().date()} {f'noisy {DoubleStainBatch.alpha}'.replace('.', 'p') if noisy_initial else ''} 10rep.json")
+        DoubleStainBatch.summary_data.to_json(f"data/DL bootstrap {datetime.today().date()} {f'noisy {DoubleStainBatch.alpha}'.replace('.', 'p') if noisy_initial else ''} base.json")
     # Saving these in json to preserve np array and not lose all readability & keep library version generality
 
-def run_bootstrap_erlang(lookup_path, jobs=1, n_steps=10, noisy_initial=False):
+def run_bootstrap_erlang(lookup_path, jobs=1, n_steps=10, noisy_initial=False, ks_as_in_lookup=True):
     edu_time = [0]
     brdu_time = list(range(1, 13, 1))
     wait_time = [0.5]
@@ -975,7 +977,7 @@ def run_bootstrap_erlang(lookup_path, jobs=1, n_steps=10, noisy_initial=False):
         x_0=np.array([0]*12*n_steps),
         extras=[jobs, n_steps]
     )
-    repeat_ensembles = [100]
+    repeat_ensembles = [200]
     # params = param_sweep_k_predefined(k_points, edu_time, brdu_time, wait_time, period, n, repeat_ensembles)
     if lookup_path[-4:] == '.csv':
         DoubleStainBatch.lookup_df = pd.read_csv(lookup_path)
@@ -983,22 +985,36 @@ def run_bootstrap_erlang(lookup_path, jobs=1, n_steps=10, noisy_initial=False):
         DoubleStainBatch.lookup_df = pd.read_json(lookup_path)
     else:
         raise ValueError('Format of lookup table not understood. Please use CSV or JSON.')
+    
+    if ks_as_in_lookup:
+        k_points = DoubleStainBatch.lookup_df[['k1', 'k2', 'k3']].copy()
+        # Take 5% of points around the mean values of k1 and k2
+        k_points = k_points[np.logical_and(np.isclose(k_points['k1'], k_points['k1'].mean(), rtol=5E-2), np.isclose(k_points['k2'], k_points['k2'].mean(), rtol=5E-2))].to_numpy()
+    else:
+        k_points = hex_param_mesh(np.array([11,8,5]), 1/np.sqrt(6)*np.array([2,-1,-1]), 1/np.sqrt(6)*np.array([-1,-1,2])*0.5, 6)
+        # k_points = hex_param_mesh(np.array([11,8,5]), 1/np.sqrt(6)*np.array([2,-1,-1]), 1/np.sqrt(6)*np.array([-1,-1,2])*0.5, 2)
+        # G1_list = np.linspace(7,17,20)
+        # S_list = np.linspace(6,12,40)
+
     if 'Wait time' not in DoubleStainBatch.lookup_df.columns:
         DoubleStainBatch.lookup_df['Wait time'] = 0.5
     if 'Cycle period' not in DoubleStainBatch.lookup_df.columns:
         DoubleStainBatch.lookup_df['Cycle period'] = 24.0
-    params = [df_row.to_dict() for _, df_row in DoubleStainBatch.lookup_df[['k1', 'k2', 'k3', 'BrdU time', 'Wait time']].iterrows()]
-    for idx in range(len(params)):
-        params[idx].update({
-            "edu_t": 0.0,
-            "brdu_t": params[idx]['BrdU time'],
-            "wait_t": params[idx]['Wait time'],
-            "cycle_period": 24.0,
-            "n": 300,
-            "repeat_ensembles": repeat_ensembles[0]
-        })
-    params = [p for p in params if np.logical_and(np.isclose(p['k1'], DoubleStainBatch.lookup_df['k1'].mean(), rtol=5E-2), np.isclose(p['k2'], DoubleStainBatch.lookup_df['k2'].mean(), rtol=5E-2))]
-    print(len(params))
+    # params = [df_row.to_dict() for _, df_row in DoubleStainBatch.lookup_df[['k1', 'k2', 'k3', 'BrdU time', 'Wait time']].iterrows()]
+    brdu_time = DoubleStainBatch.lookup_df['BrdU time'].unique()
+    params = param_sweep_k_predefined(k_points, edu_time, brdu_time, wait_time, period, n, repeat_ensembles)
+    
+    # for idx in range(len(params)):
+    #     params[idx].update({
+    #         "edu_t": 0.0,
+    #         "brdu_t": params[idx]['BrdU time'],
+    #         "wait_t": params[idx]['Wait time'],
+    #         "cycle_period": 24.0,
+    #         "n": 300,
+    #         "repeat_ensembles": repeat_ensembles[0]
+    #     })
+    # params = [p for p in params if np.logical_and(np.isclose(p['k1'], DoubleStainBatch.lookup_df['k1'].mean(), rtol=5E-2), np.isclose(p['k2'], DoubleStainBatch.lookup_df['k2'].mean(), rtol=5E-2))]
+    # print(len(params))
     params = np.random.choice(params, len(params), replace=False)
     if noisy_initial:
         DoubleStainBatch.alpha = 0.5
@@ -1026,7 +1042,7 @@ def run_demo_experiment(lookup_path='DPNL lookup.json'):
         x_0=np.array([0]*12),
         extras=[1] 
     )
-    repeat_ensembles = [3]
+    repeat_ensembles = [30]
 
     if lookup_path[-4:] == '.csv':
         DoubleStainBatch.lookup_df = pd.read_csv(lookup_path)
@@ -1062,13 +1078,15 @@ if __name__ == '__main__':
     # make_lookup_erlang(n_steps=4, noisy_initial=False)
     # df = pd.read_json('data/DL bootstrap 2025-12-20 erlang 4.json')
     # df = df.loc[np.logical_or(np.logical_or(df['SNR k1']>100.0, df['SNR k2']>100.0), np.logical_or(df['SNR k1'].isna(), df['SNR k2'].isna()))]
-    # run_bootstrap_erlang('data/DL analytic cov erlang 4.json', n_steps=4, noisy_initial=False)
+    # run_bootstrap_erlang('data/DL analytic cov erlang 4.json', n_steps=4, noisy_initial=False, ks_as_in_lookup=True)
     # print('Done!')
 
     ### Inference without a fixed period, all within 1 hour of 24 hours
 
     # run_bootstrap('data/DL analytic cov templated.json', noisy_initial=False, cov_file='data/DL analytic cov templated.json', ks_as_in_lookup=True)
-    # run_demo_experiment()
+    run_demo_experiment()
 
-    ### Increasing the number of repeats to 10
-    run_bootstrap('DPNL lookup.json', noisy_initial=False, ks_as_in_lookup=True)
+    ### Increasing the number of initial cells to 600
+    # run_bootstrap('data/DL analytic cov templated 2026-01-20.json', noisy_initial=False, ks_as_in_lookup=True)
+    ### Increasing the number of repeats per inference to 10
+    # run_bootstrap('DPNL lookup.json', noisy_initial=False, ks_as_in_lookup=True)

plotting.py
@@ -134,6 +134,8 @@ def hex_opt_plot(df, x, y, z, opt_c, opt_v, gridsize=(10, 10), normalise=False, 
 	ax.set_xlabel(x)
 	ax.set_ylabel(y)
 
+from scipy.interpolate import UnivariateSpline
+
 #%%
 def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 	'''
@@ -160,8 +162,8 @@ def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 			go.Scatter(
 				x=gdf[x], y=gdf[y],
 			  	line_shape='spline', name=str(gname),
-			  	marker=dict(size=7, color=gcol, line=dict(width=1.5)),
-				line=dict(color=gcol, width=4.0),
+			  	marker=dict(size=12, color=gcol, line=dict(width=1.8)),
+				line=dict(color=gcol, width=8.0),
 				showlegend=False,
 			)
 		)
@@ -170,20 +172,32 @@ def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 		gcol = f"rgb({gcol[0]},{gcol[1]},{gcol[2]})"
 		gdf = gdf.groupby(x).agg({x: 'first', grouping: 'first', y: 'mean'})
 		# mark peak
+		# --- mark spline extremum ---
+		xs = gdf[x].values
+		ys = gdf[y].values
+
+		# fit spline through the plotted points
+		spline = UnivariateSpline(xs, ys, s=0)
+
+		# dense grid to find true extremum
+		x_dense = np.linspace(xs.min(), xs.max(), 500)
+		y_dense = spline(x_dense)
+
 		if extremum == 'max':
-			fig.add_trace(go.Scatter(
-				x=gdf.loc[gdf[y]==gdf[y].max()][x], y=gdf.loc[gdf[y]==gdf[y].max()][y],
-				marker=dict(color=gcol, size=14), marker_symbol='star', marker_line_width=2,
-				showlegend=False
-			))
+			i = np.argmax(y_dense)
 		elif extremum == 'min':
-			fig.add_trace(go.Scatter(
-				x=gdf.loc[gdf[y]==gdf[y].min()][x], y=gdf.loc[gdf[y]==gdf[y].min()][y],
-				marker=dict(color=gcol, size=14), marker_symbol='x', marker_line_width=2,
-				showlegend=False
-			))
+			i = np.argmin(y_dense)
 		else:
-			print('Please enter either max or min to mark extrema.')
+			raise ValueError("extremum must be 'max' or 'min'")
+
+		fig.add_trace(go.Scatter(
+			x=[x_dense[i]],
+			y=[y_dense[i]],
+			marker=dict(color=gcol, size=18),
+			marker_symbol='star',
+			marker_line_width=2,
+			showlegend=False
+		))
 
 	# To make the colourscale consistent between all curves and only appear once
 	fig.add_trace(go.Scatter(
@@ -206,10 +220,10 @@ def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 		yaxis=dict(ticks='', showgrid=False, zeroline=False, nticks=11),# tickformat='.1e'),
 		autosize=False,
 		height=400,
-		width=600,
+		width=400,
 		margin=dict(l=0, r=0, t=0, b=0),
 		hovermode='closest',
-		font=dict(size=20),
+		font=dict(size=40),
 		boxmode='group',
 		legend=dict(
 			yanchor="top",
@@ -230,15 +244,15 @@ def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 		linewidth=1,
 		linecolor='black',
 		mirror=True,
-		# range=[1.0, 12.01],
+		range=[1.0, 12.01],
 		dtick=1,
 	)
-	# fig.update_layout(
-	# 	xaxis = dict(
-	# 		tickmode = 'array',
-	# 		tickvals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-	# 		ticktext=['1', '', '3', '', '5', '', '7', '', '9', '', '11', ''],		)
-	# )
+	fig.update_layout(
+		xaxis = dict(
+			tickmode = 'array',
+			tickvals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+			ticktext=['1', '', '3', '', '5', '', '7', '', '9', '', '11', ''],		)
+	)
 	fig.update_yaxes(
 		zeroline=True,
 		zerolinewidth=3,
@@ -250,50 +264,51 @@ def peak_splines(df, x, y, grouping, n_groups, colours='Blues', extremum='max'):
 		linewidth=1,
 		linecolor='black',
 		mirror=True,
-		# range=[0.0, 20.0]
+		# range=[15.0, 40.0]
+	)
+	fig.update_layout(
+		yaxis = dict(
+			tickmode = 'array',
+			tickvals = [4, 5, 6, 7, 8, 9, 10],
+			ticktext=['4', '', '6', '', '8', '', '10'],		)
 	)
 	return fig
 
 #%%
 def peak_splines_three_datasets(
-    df_solid, # 3-stage cycle
-    df_dashed, # Erlang distributed cycle
-    df_dotted, # Noisy initial conditions
+    df_solid,   # 3-stage cycle
+    df_dashed,  # Erlang distributed cycle
+    df_dotted,  # Noisy initial conditions
     x,
     y,
     colours=('black', 'black', 'black'),
+    labels=('Series 1', 'Series 2', 'Series 3'),
 ):
     """
-    Plot mean y(x) curves from three datasets using solid, dashed, and dotted lines.
-
-    Parameters
-    ----------
-    df_solid, df_dashed, df_dotted : pandas.DataFrame
-        DataFrames containing columns x and y
-    x : str
-        Column name for x-axis
-    y : str
-        Column name for y-axis
-    colours : tuple
-        Line colours for the three curves
+    Plot mean y(x) curves from three datasets using solid, dashed, and dotted lines,
+    marking the maximum of the spline-interpolated curve.
     """
 
     layout = go.Layout(plot_bgcolor='white')
     fig = go.Figure(layout=layout)
 
     datasets = [
-        (df_solid, 'solid', colours[0], '3-stage cycle'),
-        (df_dashed, 'dash', colours[1], '12-stage cycle'),
-        (df_dotted, 'dot', colours[2], '3-stage + variable initial'),
+        (df_solid,  'solid', colours[0], labels[0]),
+        (df_dashed, 'dash',  colours[1], labels[1]),
+        (df_dotted, 'dot',   colours[2], labels[2]),
     ]
 
     for df, dash_style, colour, label in datasets:
         mean_curve = df.groupby(x)[y].mean().reset_index()
 
+        xs = mean_curve[x].values
+        ys = mean_curve[y].values
+
+        # plot spline
         fig.add_trace(
             go.Scatter(
-                x=mean_curve[x],
-                y=mean_curve[y],
+                x=xs,
+                y=ys,
                 line_shape='spline',
                 name=label,
                 line=dict(
@@ -304,12 +319,17 @@ def peak_splines_three_datasets(
             )
         )
 
-        # Mark peak
-        peak_row = mean_curve.loc[mean_curve[y].idxmax()]
+        # --- spline-based peak ---
+        spline = UnivariateSpline(xs, ys, s=0)
+        x_dense = np.linspace(xs.min(), xs.max(), 500)
+        y_dense = spline(x_dense)
+
+        i_max = np.argmax(y_dense)
+
         fig.add_trace(
             go.Scatter(
-                x=[peak_row[x]],
-                y=[peak_row[y]],
+                x=[x_dense[i_max]],
+                y=[y_dense[i_max]],
                 mode='markers',
                 marker=dict(
                     symbol='star',
@@ -329,8 +349,8 @@ def peak_splines_three_datasets(
         hovermode='closest',
         font=dict(size=20),
         legend=dict(
-            yanchor="bottom",
-            y=0.05,
+            yanchor="top",
+            y=0.95,
             xanchor="right",
             x=0.95,
             bordercolor='dimgrey',
@@ -349,6 +369,7 @@ def peak_splines_three_datasets(
         linecolor='black',
         mirror=True,
         dtick=1,
+		range=[1.0,12.25]
     )
 
     fig.update_yaxes(
@@ -362,9 +383,11 @@ def peak_splines_three_datasets(
         linecolor='black',
         mirror=True,
         nticks=8,
+		range=[15.0, 55.0]
     )
 
     return fig
+
 
 # %%
 def opt_dt_vs_x(df, x, grouping, opt_indep, opt_score, colours='Blues', extra_grouping=[]):
@@ -540,6 +563,98 @@ def plotly_scatter(df, x, y, c, colourscheme='Viridis', trendline=False):
 	return fig
 
 #%%
+
+def plotly_2lines(df1, x1, y1, df2, x2, y2, colours=('red', 'blue')):
+    layout = go.Layout(
+        plot_bgcolor='white',
+        yaxis2=dict(
+            overlaying='y',
+            side='right',
+            showgrid=False,
+            zeroline=False,
+            linewidth=1.5,
+            linecolor='black',
+            mirror=True,
+        )
+    )
+
+    fig = go.Figure(layout=layout)
+
+    # First line (left y-axis)
+    fig.add_trace(
+        go.Scatter(
+            x=df1[x1],
+            y=df1[y1],
+            mode='lines',
+            name=y1,
+            line=dict(color=colours[0], width=3, dash='dot'),
+        )
+    )
+
+    # Second line (right y-axis)
+    fig.add_trace(
+        go.Scatter(
+            x=df2[x2],
+            y=df2[y2],
+            mode='lines',
+            name=y2,
+            # yaxis='y2',
+            line=dict(color=colours[1], width=3, dash='dash'),
+        )
+    )
+
+    fig.update_layout(
+        # xaxis_title=dict(text=label_lookup.get(x1, None), font=dict(size=24)),
+        yaxis_title=dict(text=y1, font=dict(size=24)),
+        yaxis2_title=dict(text=y2, font=dict(size=24)),
+        autosize=False,
+        height=400,
+        width=600,
+        margin=dict(l=0, r=0, t=0, b=0),
+        hovermode='closest',
+        font=dict(size=18),
+        legend=dict(
+            yanchor="top",
+            y=0.95,
+            xanchor="right",
+            x=0.95,
+            bordercolor='dimgrey',
+            borderwidth=2,
+        ),
+    )
+
+    fig.update_xaxes(
+        zeroline=True,
+        zerolinewidth=2,
+        zerolinecolor='black',
+        showgrid=True,
+        gridwidth=1,
+        gridcolor='gray',
+        linewidth=1.5,
+        linecolor='black',
+        mirror=True,
+        nticks=13,
+		range=[1.0, 12.01]
+    )
+
+    fig.update_yaxes(
+        zeroline=True,
+        zerolinewidth=2,
+        zerolinecolor='black',
+        showgrid=True,
+        gridwidth=1,
+        gridcolor='gray',
+        nticks=10,
+        linewidth=1.5,
+        linecolor='black',
+        mirror=True,
+		# range=[0.0, 45.0]
+    )
+
+    return fig
+
+
+#%%
 def demo_plot(df, x, y, c, lookup_df, colourscheme='Viridis'):
 	'''
 	Plot scatterpoints on a graph for x and y, color points by c and show how close they are to the ground truth
@@ -630,7 +745,7 @@ def demo_plot(df, x, y, c, lookup_df, colourscheme='Viridis'):
 		linecolor='black',
 		mirror=True,
 		# dtick=1,
-		# range=[40.0, 100.0],
+		# range=[0.0, 45.0],
 	)
 	return fig
 
@@ -700,33 +815,33 @@ DATA IMPORT PART
 #%%
 print('Reading files')
 df = pd.read_json('data/DL bootstrap 2025-12-03.json') # Mahalanobis distance used
-print('k1 values: ', df['k1'].unique(), ';', len(df['k1'].unique()), 'unique values of k1.')
-print('k2 values: ', df['k2'].unique(), ';', len(df['k2'].unique()), 'unique values of k2.')
-df['tg1'] = 1/df['k1']
-df['ts'] = 1/df['k2']
-df['tg2m'] = 1/df['k3']
-df['x'] = df['Initial G1']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
-df['y'] = df['Initial S']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
-df['z'] = df['Initial G2M']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
-for k in ['k1', 'k2']:
-	df[f'Relative error {k}'] = (df[k] - df[f'Mean inferred {k}'])/df[f'Std inferred {k}']
-df['Total cells at end'] = df['Mean EdU+BrdU+']+df['Mean EdU-BrdU+']+df['Mean EdU+BrdU-']+df['Mean EdU-BrdU-']
-df['Labelled fraction'] = 1 - df['Mean EdU-BrdU-']/df['Total cells at end']
+# print('k1 values: ', df['k1'].unique(), ';', len(df['k1'].unique()), 'unique values of k1.')
+# print('k2 values: ', df['k2'].unique(), ';', len(df['k2'].unique()), 'unique values of k2.')
+# df['tg1'] = 1/df['k1']
+# df['ts'] = 1/df['k2']
+# df['tg2m'] = 1/df['k3']
+# df['x'] = df['Initial G1']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
+# df['y'] = df['Initial S']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
+# df['z'] = df['Initial G2M']/(df['Initial G1']+df['Initial S']+df['Initial G2M'])
+# for k in ['k1', 'k2']:
+# 	df[f'Relative error {k}'] = (df[k] - df[f'Mean inferred {k}'])/df[f'Std inferred {k}']
+# df['Total cells at end'] = df['Mean EdU+BrdU+']+df['Mean EdU-BrdU+']+df['Mean EdU+BrdU-']+df['Mean EdU-BrdU-']
+# df['Labelled fraction'] = 1 - df['Mean EdU-BrdU-']/df['Total cells at end']
 
 #%%
 preprint_df = pd.read_json('DPNL inference results.json') # Can take a while for larger datasets
-print('k1 values: ', preprint_df['k1'].unique(), ';', len(preprint_df['k1'].unique()), 'unique values of k1.')
-print('k2 values: ', preprint_df['k2'].unique(), ';', len(preprint_df['k2'].unique()), 'unique values of k2.')
-preprint_df['tg1'] = 1/preprint_df['k1']
-preprint_df['ts'] = 1/preprint_df['k2']
-preprint_df['tg2m'] = 1/preprint_df['k3']
-preprint_df['x'] = preprint_df['Initial G1']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
-preprint_df['y'] = preprint_df['Initial S']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
-preprint_df['z'] = preprint_df['Initial G2M']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
-for k in ['k1', 'k2']:
-	preprint_df[f'Relative error {k}'] = (preprint_df[k] - preprint_df[f'Mean inferred {k}'])/preprint_df[f'Std inferred {k}']
-preprint_df['Total cells at end'] = preprint_df['Mean EdU+BrdU+']+preprint_df['Mean EdU-BrdU+']+preprint_df['Mean EdU+BrdU-']+preprint_df['Mean EdU-BrdU-']
-preprint_df['Labelled fraction'] = 1 - preprint_df['Mean EdU-BrdU-']/preprint_df['Total cells at end']
+# print('k1 values: ', preprint_df['k1'].unique(), ';', len(preprint_df['k1'].unique()), 'unique values of k1.')
+# print('k2 values: ', preprint_df['k2'].unique(), ';', len(preprint_df['k2'].unique()), 'unique values of k2.')
+# preprint_df['tg1'] = 1/preprint_df['k1']
+# preprint_df['ts'] = 1/preprint_df['k2']
+# preprint_df['tg2m'] = 1/preprint_df['k3']
+# preprint_df['x'] = preprint_df['Initial G1']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
+# preprint_df['y'] = preprint_df['Initial S']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
+# preprint_df['z'] = preprint_df['Initial G2M']/(preprint_df['Initial G1']+preprint_df['Initial S']+preprint_df['Initial G2M'])
+# for k in ['k1', 'k2']:
+# 	preprint_df[f'Relative error {k}'] = (preprint_df[k] - preprint_df[f'Mean inferred {k}'])/preprint_df[f'Std inferred {k}']
+# preprint_df['Total cells at end'] = preprint_df['Mean EdU+BrdU+']+preprint_df['Mean EdU-BrdU+']+preprint_df['Mean EdU+BrdU-']+preprint_df['Mean EdU-BrdU-']
+# preprint_df['Labelled fraction'] = 1 - preprint_df['Mean EdU-BrdU-']/preprint_df['Total cells at end']
 
 '''
 PLOTTING BELOW
@@ -807,3 +922,65 @@ df_ts8.plot('BrdU time', 'Mean EdU-BrdU-', kind='scatter', ax=ax, color='k')
 ax = df_ts8.plot('BrdU time', 'SNR k2', kind='scatter')
 df_ts8['Guess SNR k2'] = df_ts8['Mean EdU+BrdU+']*df_ts8['Mean EdU+BrdU-']*df_ts8['Mean EdU-BrdU+']*df_ts8['Mean EdU-BrdU-']/(df_ts8['Std EdU+BrdU+']*df_ts8['Std EdU+BrdU-']*df_ts8['Std EdU-BrdU+']*df_ts8['Std EdU-BrdU-'])
 df_ts8.plot('BrdU time', 'Guess SNR k2', kind='scatter', ax=ax, color='m')
+
+#%%
+df_600 = pd.read_json('data/DL bootstrap 2026-01-21  600init.json')
+df_10r = pd.read_json('data/DL bootstrap 2026-01-22  10rep.json')
+peak_splines_three_datasets(preprint_df[np.logical_and(np.isclose(preprint_df['k1'], preprint_df['k1'].mean(), rtol=0.05), np.isclose(preprint_df['k2'], preprint_df['k2'].mean(), rtol=0.05))], df_10r, df_600, 'BrdU time', 'SNR k2')
+
+#%%
+label_means = [f'Mean {g}' for g in gates]
+label_stds = [s.replace('Mean', 'Std') for s in label_means]
+def get_central_sensitivity(df, p='tg1', dp=0.03, label_means=label_means, rtolref=0.02, rtolpert=0.08, scale_by_param=True):
+	
+	tg1_ref = df['tg1'].mean()
+	ts_ref = df['ts'].mean()
+	
+	wait_col = 'BrdU time'
+	label_stds = [s.replace('Mean', 'Std') for s in label_means]
+
+	# --- select reference and perturbed rows ---
+	ref = df[
+		(np.isclose(df['tg1'], tg1_ref, rtol=rtolref)) &
+		(np.isclose(df['ts'], ts_ref, rtol=rtolref))
+	].copy()
+
+	if ref.empty:
+		raise ValueError('No reference mesh points found. Consider a larger rtolref.')
+	elif len(ref)>12:
+		raise ValueError('Multiple reference mesh points found. Lower rtolref.')
+
+	pert = df[
+		(np.isclose(df['tg1'], tg1_ref + (dp if p=='tg1' else 0), rtol=rtolpert)) &
+		(np.isclose(df['ts'], ts_ref + (dp if p=='ts' else 0), rtol=rtolpert))
+	].copy()
+
+	if pert.empty:
+		raise ValueError('No pertubed mesh points found. Consider a larger rtolpert.')
+	elif len(pert)>12:
+		raise ValueError('Multiple perturbed mesh points found. Lower rtolpert.')
+
+
+	ref = ref.sort_values(wait_col).set_index(wait_col)
+	pert = pert.sort_values(wait_col).set_index(wait_col)
+
+	common_times = ref.index.intersection(pert.index)
+	ref = ref.loc[common_times]
+	pert = pert.loc[common_times]
+
+	sens = (pert[label_means] - ref[label_means]) / (dp if scale_by_param else 1)
+	snr_sens = np.zeros(len(ref))
+
+	for m, s in zip(label_means, label_stds):
+		snr_sens += (sens[m] ** 2) / (ref[s] ** 2)
+
+	snr_sens = pd.Series(
+		snr_sens,
+		index=common_times,
+		name='Sensitivity ' + p
+	)
+
+	return snr_sens
+
+
+#%%